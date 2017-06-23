297e4cb541c3db2ef6ed580579798e0b56deadcf
Juan David Sanchez <juan.david748@gmail.com>
2017-06-22 21:28:42 -0500

    DP-Longest Common Subsequence

---
+++ Algorithms/String/LCS.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int LCS(string a,string b){
+  int m=a.size(),n=b.size();
+  int dp[m+1][n+1];
+  for(int i=0; i <= m; i++){
+  	for(int j=0; j <= n; j++){
+           if(i==0 || j==0){
+           	dp[i][j]=0;
+           }else if (a[i-1]== b[j-1]){
+           	 dp[i][j]=dp[i-1][j-1]+1;
+           }else{
+           	dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+           }
+  	}
+  }
+return dp[m][n];
+}
+
+
+
+int main(){
+
+  string a,b;
+  while(getline(cin,a) && getline(cin,b)){
+      cout << LCS(a,b) << endl;
+  }
+  
+	return 0;
+}
