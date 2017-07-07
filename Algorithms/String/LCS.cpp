092e9a4542a6d84b30fa50bf5ea91d95858f27dc
Juan David Sanchez <juan.david748@gmail.com>
2017-07-07 00:54:58 -0500

    DP-Printing Longest Common Subsequence

--- Algorithms/String/LCS.cpp
+++ Algorithms/String/LCS.cpp
@@ -21,12 +21,26 @@ return dp[m][n];
 
 //Printing Longest Common Subsequence
 
-void PrintLCS(int index){
-int tam=index;
-string s[index+1];
-int i = m, j = n;
-   while (i > 0 && j > 0)
-   		{
+void LCS(vector<string> a,vector<string> b){
+  int m=a.size(),n=b.size();
+  int dp[m+1][n+1];
+  for(int i=0; i <= m; i++){
+  	for(int j=0; j <= n; j++){
+           if(i==0 || j==0){
+           	dp[i][j]=0;
+           }else if (a[i-1]== b[j-1]){
+           	 dp[i][j]=dp[i-1][j-1]+1;
+           }else{
+           	dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+           }
+  	   }
+  }
+  int index=dp[m][n];
+	
+  int tam=index;
+  string s[index+1];
+  int i = m, j = n;
+  while (i > 0 && j > 0){
       if (a[i-1] == b[j-1]){
        s[index-1]=string(a[i-1]); 
        i--; j--; index--;  
@@ -40,7 +54,6 @@ int i = m, j = n;
   for(int i=0; i < tam-1; i++) cout<< s[i]+" ";
    cout <<s[tam-1] << endl;
 }
-
 
 int main(){
 
