4d15cd898ee59ef50b9edd008bbca976ffcecc37
LutzMage <lutz.mage@gmail.com>
2017-07-16 23:18:08 -0500

    Added get_powers

--- Algorithms/Number Theory/prime-factors.cpp
+++ Algorithms/Number Theory/prime-factors.cpp
@@ -197,3 +197,15 @@ ll csod(ll n) {
   }
   return ans;
 }
+
+/** EXTRA: Calculates power of p in prime factorization of n!*/
+
+int get_powers(int n, int p) {
+    int result = 0;
+    n /= p;
+    do {
+        result += n;
+        n /= p;
+    }while(n > 0);
+    return result;
+}
