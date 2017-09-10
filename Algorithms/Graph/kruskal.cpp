82d216715bf9d907c018428b3d732621f8d2b44b
Camilo Pacheco <lutz.mage@gmail.com>
2017-09-10 10:14:43 -0500

    Fixing bug in kruskal

--- Algorithms/Graph/kruskal.cpp
+++ Algorithms/Graph/kruskal.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
-#define rep(i,a,n) for(int i = a; i<n; i++)
-#define all(v,c) (v.begin(),v.end(),c)
-#define sr sort
+#define rep(i,a,n) for(int i = a; i < n; i++)
+#define MAX 200005
 
 using namespace std;
 typedef long long ll;
@@ -13,7 +12,6 @@ struct edge{
 	edge(int _s,int _t, int _w){
 		s = _s, t = _t; w = _w;
 	}
-
 	edge(){}
 };
 
@@ -21,59 +19,44 @@ struct edge{
 bool w2s (edge i,edge j) { return (i.w<j.w); }
 
 /* Disjoint set data structure for kruskal implementation */
-class DisjointSet
-{
+vi parent,ran;
+void build(int n){
+    ran.assign(n,0);
+    parent.resize(n);
+    rep(i,0,n) parent[i] = i;
+}
 
-private :
+int find_(int i) { return (parent[i] == i) ? i : (parent[i] = find_(parent[i])); }
 
-    vi parent,ran;
+bool sameSet(int i, int j) { return find_(i) == find_(j); }
 
-public :
-    DisjointSet(int n){
-        ran.assign(n,0);
-        parent.resize(n);
-        rep(i,0,n) parent[i] = i;
-    }
-
-    int findSet(int i) { return (parent[i] == i) ? i : (parent[i] = findSet(parent[i])); }
-
-    bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
-
-    void unionSet(int i, int j){
-
-        if(!isSameSet(i,j)){
-
-            int x = findSet(i), y = findSet(j);
-
-            if(ran[x]>ran[y]) parent[y] = x; 
-
-            else if(ran[x] == ran[y]) ran[y]++;
-            
+void union_(int i, int j){
+    if(!sameSet(i,j)){
+        int x = find_(i), y = find_(j);
+        if(ran[x]>ran[y]) parent[y] = x;
+        else {
+            parent[x] = y;
+            if(ran[x] == ran[y]) ran[y]++;
         }
-
     }
-};
-
+}
 
 vector<edge> graph;
 
 /* Kruskal implementation for MST */
-ll kruskal(int n){
+ll kruskal(int v){
 	ll ans = ll(0);
-	int aux = 0;
+	int put = 0;
 	edge cur;
-	DisjointSet d = DisjointSet(n);
-	sr all(graph,w2s);
-	for(int i = 0; aux < (n-1); i++){
-
+	build(v);
+    sort(graph.begin(),graph.end(),w2s);
+	for(int i = 0; put < (v-1); i++){
         cur=graph[i];
-
-        if(!d.isSameSet(cur.s,cur.t)){
-            aux++;
-            d.unionSet(cur.s, cur.t);
+        if(!sameSet(cur.s,cur.t)){
+            put++;
+            union_(cur.s, cur.t);
             ans += ll(cur.w);
         }
     }
-
     return ans;
 }
