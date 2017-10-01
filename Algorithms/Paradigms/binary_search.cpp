94bcd748e7fa227b06f1a521c9cf4c816cccfaea
Camilo Pacheco <lutz.mage@gmail.com>
2017-10-01 18:12:41 -0500

    Added FOBS

--- Algorithms/Paradigms/binary_search.cpp
+++ Algorithms/Paradigms/binary_search.cpp
@@ -7,51 +7,62 @@ vector<ll> arr;
 
 /** Find the position of x in the given array
 	If x isn't in array, returns -1 */
-long binarySearch(ll x){  
-    long l=0,r = arr.size();  
-    long mid;  
-    while(l<r){  
+long binarySearch(ll x){
+    long l=0,r = arr.size();
+    long mid;
+    while(l<r){
         mid=((l+r)>>1);
         if( x < arr[mid])r=mid;
         else l=mid+1;
     }
-    return -1;  
-}  
-
-/** Find the position of x in the given array
-	If x isn't in array, returns the position 
-	Over x should be in */
-
-long upper(ll x){  
-    long l=0,r=arr.size();  
-    long mid;  
-    while(l<r){  
-        mid=((l+r)>>1);
-        if( x < arr[mid])r=mid;
-        else l=mid+1;
-    }
-    return r;  
-}  
-
-/** Find the position of x in the given array
-	If x isn't in array, returns the position 
-	Under x must be in */
-
-long lower(ll x){  
-    long l=0,r=arr.size();  
-    long mid;  
-    while(l<r){  
-        mid=((l+r)>>1);  
-        if(x <= arr[mid])r=mid;  
-        else l=mid+1;
-    }  
-    return r;  
+    return -1;
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout << "Something to print!";
-	return 0;
+/** Find the position of x in the given array
+	If x isn't in array, returns the position
+	Over x should be in */
+
+long upper(ll x){
+    long l=0,r=arr.size();
+    long mid;
+    while(l<r){
+        mid=((l+r)>>1);
+        if( x < arr[mid])r=mid;
+        else l=mid+1;
+    }
+    return r;
+}
+
+/** Find the position of x in the given array
+	If x isn't in array, returns the position
+	Under x must be in */
+
+long lower(ll x){
+    long l=0,r=arr.size();
+    long mid;
+    while(l<r){
+        mid=((l+r)>>1);
+        if(x <= arr[mid])r=mid;
+        else l=mid+1;
+    }
+    return r;
+}
+
+/** Find the position of the first occurrence
+    of x in the given array.
+	If x isn't in array, returns -1 */
+
+long fobs(ll x) {
+    long l = 0 , r = arr.size() - 1;
+    long fo = -1, m;
+    while (l <= r) {
+        m = l + ((r - l) >> 1);
+        if (arr[m] == x) {
+            fo = m;
+            r = m - 1;
+        }
+        else if (arr[m] < x) l = m + 1;
+        else r = m - 1;
+    }
+   return fo;
 }
